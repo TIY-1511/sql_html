b736fd19e0ef184055a972c61763c41dda64f86e
trainer <trainer@mac.local>
2015-11-25 18:31:57 +0000

    Added example of inheritance

---
+++ src/inheritance.rb
@@ -0,0 +1,22 @@
+
+
+class Person
+	def speak() 
+		puts "Hiya!"
+	end
+end
+
+class Accountant < Person
+
+end
+
+accountant = Accountant.new
+
+# puts accountant.methods.sort
+# puts
+# puts accountant.methods.sort.length
+
+accountant.speak
+
+inheritance
+
